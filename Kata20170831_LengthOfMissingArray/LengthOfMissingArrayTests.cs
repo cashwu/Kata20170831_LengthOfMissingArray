79904bb4a8c2976c1071907ffefdbae07081b319
cash <cash@cashwu.com>
2017-08-31 15:35:58 +0800

    input 1,2 and 4 test case

--- Kata20170831_LengthOfMissingArray/LengthOfMissingArrayTests.cs
+++ Kata20170831_LengthOfMissingArray/LengthOfMissingArrayTests.cs
@@ -25,6 +25,12 @@ namespace Kata20170831_LengthOfMissingArray
             LengthOfMissingArrayShouldBe(2, new[] { new object[] { 1, 2, 3 }, new object[] { 1 } });
         }
 
+        [TestMethod]
+        public void input_1_2_and_4_array_should_return_3()
+        {
+            LengthOfMissingArrayShouldBe(3, new[] { new object[] { 1, 2 }, new object[] { 1 }, new object[] { 1, 2, 3, 4 } });
+        }
+
         private static void LengthOfMissingArrayShouldBe(int expected, object[][] arrayOfArrays)
         {
             var kata = new Kata();
@@ -44,7 +50,16 @@ namespace Kata20170831_LengthOfMissingArray
 
             var arrLength = arrayOfArrays.Select(arr => arr.Length).OrderBy(a => a).ToList();
 
-            return arrLength[1] - arrLength[0];
+            var result = 0;
+            for (var i = 0; i < arrLength.Count; i++)
+            {
+                if (i + 1 < arrLength.Count)
+                {
+                    result = arrLength[i + 1] - arrLength[i] > 1 ? arrLength[i] + 1 : 0;
+                }
+            }
+
+            return result;
         }
     }
 }
