119d571acfe96d0fa4f7835117b3ff6a08a9300f
cash <cash@cashwu.com>
2017-08-31 14:58:10 +0800

    input object[][]{} test case

--- Kata20170831_LengthOfMissingArray/LengthOfMissingArrayTests.cs
+++ Kata20170831_LengthOfMissingArray/LengthOfMissingArrayTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170831_LengthOfMissingArray
     public class LengthOfMissingArrayTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_empty_array_should_return_0()
         {
+            LengthOfMissingArrayShouldBe(0, new object[][] { });
+        }
+
+        private static void LengthOfMissingArrayShouldBe(int expected, object[][] arrayOfArrays)
+        {
+            var kata = new Kata();
+            var actual = kata.GetLengthOfMissingArray(arrayOfArrays);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public int GetLengthOfMissingArray(object[][] arrayOfArrays)
+        {
+            return 0;
         }
     }
 }
