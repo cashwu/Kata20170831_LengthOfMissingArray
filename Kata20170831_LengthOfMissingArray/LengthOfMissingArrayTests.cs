159f22baef2ed4167fa4e3d158808f98e5113b92
cash <cash@cashwu.com>
2017-08-31 14:49:47 +0800

    init

--- Kata20170831_LengthOfMissingArray/LengthOfMissingArrayTests.cs
+++ Kata20170831_LengthOfMissingArray/LengthOfMissingArrayTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170831_LengthOfMissingArray
 {
     [TestClass]
-    public class UnitTest1
+    public class LengthOfMissingArrayTests
     {
         [TestMethod]
         public void TestMethod1()
